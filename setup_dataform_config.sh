7ad9aec0b870874ff2845e7d7a0dff136c96d99e
le.fu <le.fu@pwc.com>
2024-03-14 15:42:18 +0000

    modify cd

---
+++ setup_dataform_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ENVIRONMENT="$1"
+
+if [ "$ENVIRONMENT" = "dev" ]; then
+  DEFAULT_DATABASE="pg-jp-n-app-181481"
+  DEFAULT_LOCATION="asia-northeast1"
+else
+  DEFAULT_DATABASE="pg-jp-n-app-637776"
+  DEFAULT_LOCATION="asia-northeast1"
+fi
+
+cat <<EOF > dataform.json
+{
+  "defaultSchema": "Mart_demo_data",
+  "assertionSchema": "dataform_assertions",
+  "warehouse": "bigquery",
+  "defaultDatabase": "$DEFAULT_DATABASE",
+  "defaultLocation": "$DEFAULT_LOCATION"
+}
+EOF
